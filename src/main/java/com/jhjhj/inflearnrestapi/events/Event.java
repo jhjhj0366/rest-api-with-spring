606de2d04437295eb392a3e2c31a3e7b35c32eda
bestdeveloper0366 <bestdeveloper0366@gmail.com>
2019-12-29 15:48:37 +0900

    Event Domain

---
+++ src/main/java/com/jhjhj/inflearnrestapi/events/Event.java
@@ -0,0 +1,31 @@
+package com.jhjhj.inflearnrestapi.events;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+@EqualsAndHashCode(of="id")         // entity 간의 연관관계가 있을 떄, stackoverflow 를 방지
+
+@Data
+public class Event {
+
+    private Integer id;
+    private String name;
+    private String description;
+    private LocalDateTime beginEnrollmentDateTime;
+    private LocalDateTime closeEnrollmentDateTime;
+    private LocalDateTime beginEventDateTime;
+    private LocalDateTime endEventDateTime;
+    private String location; // (optional) 이게 없으면 온라인 모임
+    private int basePrice; // (optional)
+    private int maxPrice; // (optional)
+    private int limitOfEnrollment;
+    private boolean offline;
+    private boolean free;
+    private EventStatus eventStatus;
+
+}
