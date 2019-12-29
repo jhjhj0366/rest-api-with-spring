40810485fab60fc353c825e4de36511dcdd6f894
bestdeveloper0366 <bestdeveloper0366@gmail.com>
2019-12-29 22:36:10 +0900

    fail on unknown properties

--- src/test/java/com/jhjhj/inflearnrestapi/events/EventControllerTests.java
+++ src/test/java/com/jhjhj/inflearnrestapi/events/EventControllerTests.java
@@ -39,6 +39,36 @@ public class EventControllerTests {
     @Test
     public void createEvent() throws Exception {
 
+        EventDto event = EventDto.builder()
+                .name("spirng")
+                .description("Spring")
+                .beginEventDateTime(LocalDateTime.of(2019, 12, 29, 16, 30))
+                .closeEnrollmentDateTime(LocalDateTime.of(2019, 12, 29, 16, 30))
+                .beginEventDateTime(LocalDateTime.of(2019, 12, 29, 16, 30))
+                .closeEnrollmentDateTime(LocalDateTime.of(2019, 12, 29, 16, 30))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타트업 팩토리")
+                .build();
+
+        mockMvc.perform(post("/api/events/")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+//                .andExpect(jsonPath("id").exists())
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+                .andExpect(jsonPath("id").value(Matchers.not(100)))
+                .andExpect(jsonPath("free").value(Matchers.not(true)))
+                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+    }
+
+    @Test
+    public void createEvent_Bad_Request() throws Exception {
+
         Event event = Event.builder()
                 .id(100)
                 .name("spirng")
@@ -53,7 +83,6 @@ public class EventControllerTests {
                 .limitOfEnrollment(100)
                 .location("강남역 D2 스타트업 팩토리")
                 .free(true)
-                .offline(false)
                 .eventStatus(EventStatus.PUBLISHED)
                 .build();
 
@@ -62,11 +91,7 @@ public class EventControllerTests {
                 .accept(MediaTypes.HAL_JSON)
                 .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
-                .andExpect(jsonPath("id").value(Matchers.not(100)))
-                .andExpect(jsonPath("free").value(Matchers.not(true)))
-                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()));
+                .andExpect(status().isBadRequest())
+        ;
     }
 }
